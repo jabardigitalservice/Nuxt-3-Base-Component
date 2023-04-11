0bb5562c11c6fe3ee12da4a7ee79f133f66ffb81
maruf12 <maruf.harsono@gmail.com>
2023-04-11 14:29:10 +0700

    chore: setup lint fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: ["@nuxt/eslint-config"],
+  rules: {
+    "vue/multi-word-component-names": [
+      "error",
+      {
+        ignores: ["index", "login"],
+      },
+    ],
+  },
 };
